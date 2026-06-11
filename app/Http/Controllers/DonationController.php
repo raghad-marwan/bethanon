5045f58176856b4e66903148ae4edb36e2e688ee
Raghad Abu Dawabeh <youremail@example.com>
2026-06-11 16:04:58 +0300

    first commit

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -49,7 +49,7 @@ class DonationController extends Controller
                 'name' => 'محفظة مالتشات',
                 'iban' => '0594394229',
                 'bank_name' => 'محفظة مالتشات',
-                'account_name' => 'صندوق مساعدة الناس - بيت حانون',
+                'account_name' =>  'صندوق بيت حانون تاتكافلي المستدام',
             ],
         ];
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>صندوق مساعدة الناس - غزة</title>
+    <title> صندوق بيت حانون التكافلي المستدام</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
